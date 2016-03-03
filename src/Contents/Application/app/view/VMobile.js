b6f59db64612170cad08487717751a760890d1ce
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-03 15:55:29 +0100

    Update# 69

--- src/Contents/Application/app/view/VMobile.js
+++ src/Contents/Application/app/view/VMobile.js
@@ -44,7 +44,7 @@ App.view.define('VMobile', {
                 margin: {
                     top: 10,
                     bottom: 0,
-                    left: 137,
+                    left: 500,
                     right: 0
                 }
             }
