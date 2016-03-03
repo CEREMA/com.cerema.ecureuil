eb2ee2992d54c311b8eee20b40f9be56a11ab266
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-03 15:20:25 +0100

    Update# 33

--- src/Contents/Application/app/view/VMobile.js
+++ src/Contents/Application/app/view/VMobile.js
@@ -441,4 +441,62 @@ App.view.define('VMobile', {
 });
 
 
-/*
+/*{
+        this.width = 300;
+        this.height = 150;
+        this.title = "Ajouter un périphérique";
+        this.bodyCls = "white";
+        this.items = [
+            {
+                layout: "hbox",
+                border: false,
+                width: "100%",
+                items: [
+            		{
+                        xtype: "textfield",
+                        itemId: "Peripherique",
+                        margin: 10,
+                        flex: 1,
+                        labelAlign: "top",
+                        allowBlank: false,
+                        fieldLabel: "Code de synchronisation",
+                        labelWidth: 200,
+                    }
+				]
+            }, {
+                layout: "hbox",
+                width: 400,
+                height: 4,
+                margin: {
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0
+                },
+                border: false,
+                items: [
+
+                ]
+            }
+        ];
+        this.bbar = [{
+                xtype: "button",
+                text: "Annuler",
+                itemId: "CANCEL_lien"
+            }, 
+            {
+                xtype: "button",
+                itemId: "valider_saisie",
+                text: "Enregistrer",
+                margin: {
+                    top: 10,
+                    bottom: 0,
+                    left: 150,
+                    right: 0
+                }
+            }
+        ];
+        this.callParent(arguments);
+    }
+
+});*/
