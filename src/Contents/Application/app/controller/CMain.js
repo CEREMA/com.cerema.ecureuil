90a553e9feb2181ea4605c2c147502762b7453f4
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-03 15:27:47 +0100

    Update# 41

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -362,7 +362,7 @@ App.controller.define('CMain', {
         };
     },
     annulerMobile: function(p, record) {
-        App.get('MobileADD').close();
+        App.get('VMobile').close();
     },
 /*****************************************************************************************************************************************************************/
     //remet les saisies à vide
