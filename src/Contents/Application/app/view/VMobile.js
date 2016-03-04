3e843074f4cbc10a2480d5fa4a6d4d3dbae74c83
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-04 11:40:15 +0100

    Update# 108

--- src/Contents/Application/app/view/VMobile.js
+++ src/Contents/Application/app/view/VMobile.js
@@ -24,7 +24,7 @@ App.view.define('VMobile', {
                         margin: 10,
                         allowBlank: false,
                         fieldLabel: "Code de synchronisation",
-                        labelWidth: 100
+                        labelWidth: 200
                     }
 				]
             }
