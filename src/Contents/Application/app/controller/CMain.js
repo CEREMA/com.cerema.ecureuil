8b1e80590b0ab57c4ddb24c5fd47b617741258e5
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-04 13:56:09 +0100

    Update# 165

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -379,6 +379,7 @@ App.controller.define('CMain', {
                             MobileId: mobileId
                         },function(e,r) {
                             if(r.result.affectedRows == 1) {
+                                App.get('VMobile label#LabelError').style([{style: {color: 'green'} }]);
                                 App.get('VMobile label#LabelError').setText('Synchronisation Terminée.');
                             }
                         });
