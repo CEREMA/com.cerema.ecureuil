e9e80887edb18449b744235fa23dfc51f0a64019
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-04 15:23:47 +0100

    Update# 234

--- src/Contents/Application/app/view/VMobile.js
+++ src/Contents/Application/app/view/VMobile.js
@@ -20,6 +20,7 @@ App.view.define('VMobile', {
                                     '<div class="title-step">Etape 1 : Télécharger l\'application "Ecureuil Mobile"</div>'+
                                     '<div class="googleplay"></div><div class="appstore"></div>'+
                                     '<div class="title-step">Etape 2 : Suivre les instructions sur votre mobile</div>'+
+                                    '<div class="title-step">Après l\'installation de l\'application.<br/> Vous vous laissez guider par les étape de synchronisation</div>'+
                                 '</div>',
                         flex: 11
                     },
