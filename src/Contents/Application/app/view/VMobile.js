ff4500c150dcd968046c414c391161b2a98d07ed
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-04 14:23:31 +0100

    Update# 183

--- src/Contents/Application/app/view/VMobile.js
+++ src/Contents/Application/app/view/VMobile.js
@@ -15,7 +15,7 @@ App.view.define('VMobile', {
                     {
                         width: "100%",
                         html: '<div>Ecureuil Mobile</div><div>Image</div><div></div>',
-                        flex: 13
+                        flex: 10
                     },
             		{
                         flex: 1,
