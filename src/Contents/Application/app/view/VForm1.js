6d84933e6a3704e14806b459deefbadad4f38c8a
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-22 11:00:52 +0100

    Update# 264

--- src/Contents/Application/app/view/VForm1.js
+++ src/Contents/Application/app/view/VForm1.js
@@ -62,52 +62,6 @@ App.view.define('VForm1',{
 		store: App.store.create('App.AO.getAll',{
 			groupField: 'nom_thematique'
 		})
-		,
-		features: [
-		{
-			groupHeaderTpl: '{name}',
-			ftype: 'groupingsummary'
-		}
-		],
-		columns: [
-			{
-				text: "Id",
-				dataIndex: "IdAppelOffre",
-				width: 50
-			},			{
-				text: "Objet",
-				dataIndex: "Objet",
-				flex: 1
-			},
-			{
-				text: "Thématique",
-				dataIndex: "nom_thematique",
-				width: 300,
-				hidden: true				
-			},
-			{
-				text: "Client",
-				dataIndex: "Client",
-				width: 200				
-			},
-			{
-				text: "Date de parution",
-				dataIndex: "DateParution",
-				type: "date",
-				renderer:  Ext.util.Format.dateRenderer('d/m/Y')
-			},
-			{
-				text: "Date limite",
-				dataIndex: "DateLimite",
-				type: "date",
-				renderer: function(value) {
-					return '<div style="color:red">'+value.toString('dd/MM/yyyy')+'</div>'
-				}
-			}
-		],
-		store: App.store.create('App.AO.getAll',{
-			groupField: 'nom_thematique'
-		})
 	}	
 	]
 });
