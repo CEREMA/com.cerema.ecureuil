7a192a2a19f7b882e5b9215117f4b12b179fde8c
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-22 13:18:07 +0100

    Update# 313

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -369,7 +369,7 @@ App.controller.define('CMain', {
         App.AO.getFavoris(null,function(e, r){
             console.log(e);
             console.log(r);
-            console.log(App.AO.getFavoris);
+            console.log(App.AO.getFavoris());
 
             App.get('TFavoris grid#AO').bindStore(App.store.create('App.AO.getFavoris',{groupField: 'nom_thematique'}));
             App.get('TFavoris grid#AO').getStore().load();
