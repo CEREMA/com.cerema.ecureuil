9398426abe03e4de95eefcf0d8b8232dd83edbc6
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-23 09:48:07 +0100

    Update# 405

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -368,11 +368,21 @@ App.controller.define('CMain', {
     },
 /*****************************************************************************************************************************************************************/
     SuppressionFavori: function(){
-        console.log('SuppressionFavori');
-        var result = confirm("Êtes-vous sûr de vouloir supprimer cet appel d'offre de vos favoris ?");
-        if (result) {
-            //Logic to delete the item
-        }
+        Ext.Msg.show({
+            title:'Save Changes?',
+            message: 'You are closing a tab that has unsaved changes. Would you like to save your changes?',
+            buttons: Ext.Msg.YESNOCANCEL,
+            icon: Ext.Msg.QUESTION,
+            fn: function(btn) {
+                if (btn === 'yes') {
+                    console.log('Yes pressed');
+                } else if (btn === 'no') {
+                    console.log('No pressed');
+                } else {
+                    console.log('Cancel pressed');
+                } 
+            }
+        });
     },
     ConsultationFavoris: function(p, record){
         console.log('ConsultationFavoris');
