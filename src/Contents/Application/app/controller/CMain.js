cbb2023abf18f02df335a1234ee3b299942e0bc9
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-23 10:04:55 +0100

    Update# 413

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -391,7 +391,7 @@ App.controller.define('CMain', {
         });
     },
     ConsultationFavoris: function(p, record){
-
+        console.log(record);
         OP = false;
 
         App.view.create('VAppelOffreFavoris', {
