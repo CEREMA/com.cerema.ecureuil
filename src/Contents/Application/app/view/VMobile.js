3473f3162d4eab0afdf397ee876a16297ec58b80
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-30 09:36:53 +0200

    Update# 564

--- src/Contents/Application/app/view/VMobile.js
+++ src/Contents/Application/app/view/VMobile.js
@@ -40,7 +40,7 @@ App.view.define('VMobile', {
                                 fieldLabel: "Code de synchronisation",
                                 labelWidth: 175,
                                 labelStyle: 'font-size: 1.2em;',
-                                style: 'font-size: 1.2em;'
+                                style: 'line-height: 1.2em;'
                             },
                             {   
                                 flex: 2,
