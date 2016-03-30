0384fb35e9209b7de6ced279ceb1bf1fa41f208f
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-30 09:57:08 +0200

    Update# 589

--- src/Contents/Application/app/view/VMobile.js
+++ src/Contents/Application/app/view/VMobile.js
@@ -3,7 +3,7 @@ App.view.define('VMobile', {
     alias: "widget.VMobile",
     initComponent: function() {
         this.width = 600;
-        this.height = 450;
+        this.height = 425;
         this.title = "Ajouter un périphérique";
         this.bodyCls = "white";
         this.layout="fit";
