5a12733135d1eb2b6967268df03195daed8e44f3
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-30 09:53:37 +0200

    Update# 584

--- src/Contents/Application/app/view/VMobile.js
+++ src/Contents/Application/app/view/VMobile.js
@@ -41,7 +41,7 @@ App.view.define('VMobile', {
                                 labelWidth: 175,
                                 height: 40,
                                 labelStyle: {
-                                    'font-size': '30px'
+                                    'fontSize': '30px'
                                 },
                                 fieldStyle: {
                                     'fontSize' : '30px'
