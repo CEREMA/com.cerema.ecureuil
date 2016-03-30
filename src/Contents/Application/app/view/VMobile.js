1deba7ba05f752bbfc24eef68586cac480f1f440
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-30 09:57:48 +0200

    Update# 590

--- src/Contents/Application/app/view/VMobile.js
+++ src/Contents/Application/app/view/VMobile.js
@@ -30,6 +30,9 @@ App.view.define('VMobile', {
                         flex: 2,
                         layout: "hbox",
                         width: 600,
+                        margin: {
+                            top:10
+                        },
                         items: [
                             {   
                                 xtype: "textfield",
