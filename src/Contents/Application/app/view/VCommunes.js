e6b6a60959ae468c9daa1233881a4b7b317a2c5b
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-11 09:33:32 +0200

    Update# 652

--- src/Contents/Application/app/view/VCommunes.js
+++ src/Contents/Application/app/view/VCommunes.js
@@ -2,8 +2,8 @@ App.view.define('VCommunes', {
     extend: "Ext.window.Window",
     alias: "widget.VCommunes",
     initComponent: function() {
-        this.width = 1024;
-        this.height = 868;
+        this.width = 800;
+        this.height = 650;
         this.title = "Consultation de l'appel d'offre";
         this.bodyCls = "white";
 		this.layout="border";
