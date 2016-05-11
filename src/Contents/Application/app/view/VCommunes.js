2f7f0243f08d2f44a931ff06bba4f17b7e6a2275
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-11 09:23:12 +0200

    Update# 647

--- src/Contents/Application/app/view/VCommunes.js
+++ src/Contents/Application/app/view/VCommunes.js
@@ -8,15 +8,12 @@ App.view.define('VCommunes', {
         this.bodyCls = "white";
 		this.layout="fit";
         this.items = [
-		{
-			layout: "fit",
-			items: [
-{ 
-      xtype: 'component', 
-      // some long html to demonstrate scrolling
+		
+
+	{ 
       html: '<div style="background-color:red" width="100%" height="600px" id="TMapPanel">&nbsp;</div>'
-    }			]
-		}        
+    }			
+		        
 		];
         this.bbar = [ 
         ];
