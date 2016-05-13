da5b077a3246844e247b74eb323b9652ed646d6c
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-13 15:29:54 +0200

    Update# 801

--- src/Contents/Application/app/view/VForm4.js
+++ src/Contents/Application/app/view/VForm4.js
@@ -1,52 +1,42 @@
-App.view.define('VForm4',{
-	extend: "Ext.window.Window",
-	width: 465,
-	height: 410,
-	title: "Notification Groupe",
-	alias: "widget.TForm4",
+App.view.define('VForm4', {
+    extend: "Ext.window.Window",
+    width: 465,
+    height: 410,
+    title: "Notification Groupe",
+    alias: "widget.TForm4",
     layout: "fit",
-	items: [
-					
-					{
-					xtype: "grid",
-					itemId: "grid3",
-					margin: {
-								top: 10,
-								bottom: 0,
-								left:159,
-								right: 0
-							},
-					
-					columns: [
-						{
-							text: "Nom groupe",
-							width: 120,
-							flex:1,
-							dataIndex: "LibUnic",
-							
-						},
-						
-					],
-					store: App.store.create('App.AO.getUnites', // Creation du store
-												{
-													autoLoad: true
-												}) 
-					
-					
-				}        
-	],
-	 bbar: [
-				{
-					xtype: "button",
-					text: "Annuler",
-					itemId: "annuler2"
-				},
-		'->',
-				{
-					xtype: "button",
-					text: "Valider",
-					itemId: "ajouter2"
-					
-				}
-					], 
-	});
+    items: [
+
+        {
+            xtype: "grid",
+            itemId: "grid3",
+            columns: [{
+                    text: "Nom groupe",
+                    width: 120,
+                    flex: 1,
+                    dataIndex: "LibUnic",
+
+                },
+
+            ],
+            store: App.store.create('App.AO.getUnites', // Creation du store
+                {
+                    autoLoad: true
+                })
+
+
+        }
+    ],
+    bbar: [{
+            xtype: "button",
+            text: "Annuler",
+            itemId: "annuler2"
+        },
+        '->', {
+            xtype: "button",
+            text: "Valider",
+            itemId: "ajouter2"
+
+        }
+    ],
+});
