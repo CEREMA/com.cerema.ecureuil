8a9e81dea77e596624b300de3211312341d6cbd1
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-13 15:24:03 +0200

    Update# 800

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -167,6 +167,7 @@ App.controller.define('CMain', {
                 "click": function(p) {
                     var s = App.get(p.up('window'),"grid#search").getSelectionModel().getSelection();
                     if (s) s=s[0].data;
+                    App.DB.post('gestionao2://my_communes',s);
                     App.get(p.up('window'),'grid#gridcom').getStore().add();
                     var data=App.get(p.up('window'),'grid#gridcom').getStore().getRange();
                     data.push({
