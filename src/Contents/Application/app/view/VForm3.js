6eebcf7a441a33387bc683ba05c629cfedff9704
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-13 15:40:42 +0200

    Update# 805

--- src/Contents/Application/app/view/VForm3.js
+++ src/Contents/Application/app/view/VForm3.js
@@ -2,7 +2,7 @@ App.view.define('VForm3',{
 	extend: "Ext.window.Window",
 	width: 610,
 	height: 640,
-	title: "Notification utilisateurs",
+	title: "Notification agents",
 	alias: "widget.TForm3",
     layout: "fit",
 	items: [
@@ -13,30 +13,29 @@ App.view.define('VForm3',{
 					items: [
 					
 					{
-					xtype: "grid",
-					itemId: "grid2",
-                    border: false,
-					columns: [
-						{
-							text: "Nom",
-							width: 120,
-							dataIndex: "Nom",
+                        xtype: "grid",
+                        itemId: "grid2",
+                        border: false,
+                        columns: [
+                            {
+                                text: "Nom",
+                                width: 120,
+                                dataIndex: "Nom",
+
+                            },
+                            {
+                                text: "Prenom",
+                                dataIndex: "Prenom",
+                                flex:1
+                            },
+                        ],
+                        store: App.store.create('App.AO.getAgents',
+                         {
+                            autoLoad: true
+                        }) 										
+				    }
 							
-						},
-						{
-							text: "Prenom",
-							dataIndex: "Prenom",
-							flex:1
-						},
-					],
-					store: App.store.create('App.AO.getAgents',
-					 {
-						autoLoad: true
-					}) 
-					
-					
-				},
-							]		
+            ]		
 		},
 	],
 	 bbar: [
