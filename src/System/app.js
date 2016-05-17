3e0823a7c9c02f251b87a03c54643eeb24fed572
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-17 11:38:53 +0200

    Update# 818

--- src/System/app.js
+++ src/System/app.js
@@ -1,10 +1,7 @@
 App = {
 	init: function(app,server) {
 		app.use('/tmp',server.static(__dirname + require('path').sep+'tmp'));		
-		app.post('/upload',function(req,res) {
-			App.upload.up(req,res);
-		});
-		app.post('/',function(req,res) {
+		app.post('/',app.UPLOAD.any(),function(req,res,next) {
 			App.upload.up(req,res);
 		});
 		app.get('/docs/*',function(req,res) {
