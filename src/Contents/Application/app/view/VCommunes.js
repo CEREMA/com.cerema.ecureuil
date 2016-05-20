7a56e542e6a8ea0995a7a16599dbc1d281464b27
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-20 09:43:18 +0200

    Update# 855

--- src/Contents/Application/app/view/VCommunes.js
+++ src/Contents/Application/app/view/VCommunes.js
@@ -18,57 +18,6 @@ App.view.define('VCommunes', {
         }
         ];
         this.items = [
-            {
-                collapsible: false,
-                region:'west',
-                xtype: "grid",
-                itemId: "search",
-                bbar: [
-                '->',
-                {
-                    text: "Ajouter",
-                    itemId: "Add_commune"
-                }
-                ],
-                verticalScroller: {
-                    xtype: 'paginggridscroller',
-                    activePrefetch: false
-                },
-                columns: [{
-                    header: "CPostal",
-                    width: 80,
-                    dataIndex: "codes_postaux"
-                },{
-                    header: "Commune",
-                    flex: 1,
-                    dataIndex: "nom_commune"
-                },{
-                    header: "Région",
-                    width: 250,
-                    dataIndex: "nom_region"
-                }],
-                store: App.store.create({fields:[],data:[]}),
-                height: 200
-            },
-            {
-			region:'east',
-			width: 200,
-            layout: "fit",
-            border: false,
-            items: [
-            {
-                xtype: "grid",
-                itemId: "gridcom",
-                border: false,
-                columns: [{
-                    header: "Commune",
-                    dataIndex: "nom_commune",
-                    flex: 1
-                }],
-                store: App.store.create("gestionao2://my_communes_fields",{autoLoad: true})
-            }
-            ]
-		    } 		
 		];
         this.bbar = [ 
             '->',
