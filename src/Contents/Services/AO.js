f284fa417046bcc67b670b53bb91f6d43b13caa6
Stéphane Zucatti <stephane.zucatti@me.com>
2016-06-14 10:41:45 +0200

    Update# 998

--- src/Contents/Services/AO.js
+++ src/Contents/Services/AO.js
@@ -6,7 +6,7 @@ AO = {
 		AO.using('db').model('gestionao2','select * from appelsoffres left join sources on appelsoffres.IdSource=sources.IdSource left join consultations on appelsoffres.IdConsultation=consultations.IdConsultation left join departements on appelsoffres.IdDepartement=departements.IdDepartement left join naturesprestations on appelsoffres.IdNaturePrestation=naturesprestations.IdNaturePrestation left join domaine on appelsoffres.IdDomaine=domaine.id_domaine left join thematiques on appelsoffres.IdThematique=thematiques.id_thematique where IdAppelOffre="'+o+'"',cb);
 	},
 	getAll: function(o,cb) {
-		AO.using('db').model('gestionao2','SELECT * FROM (appelsoffres LEFT JOIN domaine ON (domaine.id_domaine = appelsoffres.IdDomaine)) where YEAR(DateParution)>=2014 order by DateParution desc',cb);		
+		AO.using('db').model('gestionao2','SELECT *,domaine.* FROM (appelsoffres LEFT JOIN domaine ON (domaine.id_domaine = appelsoffres.IdDomaine)) where YEAR(DateParution)>=2014 order by DateParution desc',cb);		
 	},
 	getProfil: function(o,cb) {
 		AO.using('db').query('gestionao2','select * from profils where PassportUID="'+o+'"',cb);
