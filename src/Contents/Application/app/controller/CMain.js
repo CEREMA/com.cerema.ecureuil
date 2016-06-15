2b693234e6bd594a78559705090b0afa59151c47
Stéphane Zucatti <stephane.zucatti@me.com>
2016-06-15 16:45:54 +0200

    Update# 1039

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -273,7 +273,6 @@ App.controller.define('CMain', {
             "TConsult grid#TCommunes": {
                 beforeitemcontextmenu: "gridCommunes_oncontextmenu"
 			}
-            }
         });
 
         App.init('VMain', this.onLoad);
