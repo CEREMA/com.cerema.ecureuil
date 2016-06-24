6a3a72a6a29e27182f1957289d2d465fcc92a1cb
Stéphane Zucatti <stephane.zucatti@me.com>
2016-06-24 10:50:18 +0200

    Update# 1140

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -423,7 +423,6 @@ App.controller.define('CMain', {
                         tabx.push(parseInt(tab[i]));
                     };
                     
-                    alert(record.data.Communes);
                     var store=App.store.create('gestionao2://communes{ville_id,ville_nom}?ville_id='+record.data.Communes);
                     App.get('TConsult grid#TCommunes').bindStore(store);
                     App.get('TConsult grid#TCommunes').getStore().load();
